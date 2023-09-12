897e83c09c7db5bb6f3b70518dec228cd94aefcb
aditya-gupta-dev <142041364+aditya-gupta-dev@users.noreply.github.com>
2023-09-12 20:14:38 +0530

    Create Footer.js

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <>
+            <div className="text-center py-3 text-warning">
+                &copy; {new Date().getFullYear()} Aditya gupta: Thanks for visiting my page :)
+            </div>
+        </>
+    );
+}
