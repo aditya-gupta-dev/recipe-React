0d3323263ac99230969d4293abe8e04f3ed523db
aditya-gupta-dev <142041364+aditya-gupta-dev@users.noreply.github.com>
2023-09-12 20:15:36 +0530

    Basic Skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import Footer from './Footer';
+import Title from './Title';
+import './App.css';
+import Recipe from './Recipe';
 
 function App() {
   return (
     <>
-        <h1>this is react</h1>
+        <div className="App">
+      <Title />
+      <form className="search-form">
+        <input className="search-bar" type="text" placeholder="Enter your favourite dish, I'm sure we have it! :)"/> {/*passes the onChange event to updateSearch, it needs to be here so can retrieve input value (as opposed to button)*/}
+        <button className="search-button" type="submit">Search</button>
+      </form>
+      <div className="recipe">
+        <Recipe image='alt' calories={200} ingredients={[{text: "something"}]} title='title'/>
+      </div>
+      <Footer />
+    </div>
     </>
   );
 }
