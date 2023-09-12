e61eb933b19f9a4c6fca88679253de59c2662221
aditya-gupta-dev <142041364+aditya-gupta-dev@users.noreply.github.com>
2023-09-12 20:27:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Footer from './Footer';
 import Title from './Title';
 import './App.css';
 import Recipe from './Recipe';
 
 function App() {
-  return (
-    <>
-        <div className="App">
-      <Title />
-      <form className="search-form">
-        <input className="search-bar" type="text" placeholder="Enter your favourite dish, I'm sure we have it! :)"/> {/*passes the onChange event to updateSearch, it needs to be here so can retrieve input value (as opposed to button)*/}
-        <button className="search-button" type="submit">Search</button>
-      </form>
-      <div className="recipe">
-        <Recipe image='alt' calories={200} ingredients={[{text: "something"}]} title='title'/>
-      </div>
-      <Footer />
-    </div>
-    </>
-  );
+    const APP_ID = 'YOUR_ID';
+    const APP_KEY = 'YOUR_KEY';
+
+    const [recipes, setRecipes] = useState([]);
+    const [search, setSearch] = useState("");
+    const [query, setQuery] = useState("");
+
+    const getRecipes = async () => {
+        const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+        const data = await response.json();
+        setRecipes(data.hits);
+    }
+
+    const updateSearch = (e) => {
+        setSearch(e.target.value);
+    }
+
+    const getSearch = (e) => {
+        e.preventDefault();
+        setQuery(search);
+        setSearch("");
+    }
+
+    const buttonOver = (e) => {
+        e.target.innerHTML = "Go!";
+    }
+    const buttonOut = (e) => {
+        e.target.innerHTML = "Search";
+    }
+
+    useEffect(() => {
+        getRecipes();
+    }, [query]);
+
+    return (
+        <>
+            <div className="App">
+                <Title />
+                <form onSubmit={getSearch} className="search-form">
+                    <input className="search-bar" type="text" placeholder="Enter your favourite dish, I'm sure we have it! :)" value={search} onChange={updateSearch} /> {/*passes the onChange event to updateSearch, it needs to be here so can retrieve input value (as opposed to button)*/}
+                    <button className="search-button" type="submit" onMouseOver={buttonOver} onMouseOut={buttonOut}>Search</button>
+                </form>
+                <div className="recipe">
+                    {recipes.map((recipe, index) => (
+                        <Recipe
+                            key={index} 
+                            title={recipe.recipe.label}
+                            calories={recipe.recipe.calories}
+                            image={recipe.recipe.image}
+                            ingredients={recipe.recipe.ingredients}
+                        />
+                    ))};
+            </div>
+                <Footer />
+            </div>
+        </>
+    );
 }
 
 export default App;
